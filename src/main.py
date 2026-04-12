minor fixes + combined density and bead spread calculation

src/main.py
@@ -9,7 +9,6 @@ from patch_computer import calc_bead_spread, get_patches, annotate_patches
 from pdb_parser import parse_all_struct
 from utils import _get_bounding_box
 import argparse
-from ihm_parser import *
 import tqdm
 
 def main_density_calc(i, coords, mass, radius, grid, voxel_size, n_breaks):
@@ -38,17 +37,114 @@ def get_file_type(input):
 		i = i+1
 	return file_type
 
+def get_bead_spread(i, coords, mass, radius, grid, voxel_size, n_breaks):
+	# dummy function to call main_density_calc for parallelization
+	density = main_density_calc(i, coords, mass, radius, grid, voxel_size, n_breaks)
+	spread = calc_bead_spread(density, grid)
+	return spread
+
+def run_prism( coords, mass, radius, ps_names, args, output_dir = None ):
+
+	if output_dir is None:
+		output_dir = args.output
+
+	models = round(args.models*coords.shape[0])
+	if args.models != 1:
+		selected_models = np.random.choice(coords.shape[0], models, replace=False)
+		coords = coords[selected_models]
+	print("Number of Models = {}".format(coords.shape[0]))
+	print("Number of Beads = {}".format(coords.shape[1]))
+
+	# Create the grid.
+	grid = SparseGrid(voxel_size=args.voxel_size)
+	grid.create_grid(coords)
+	# Not padding the grid.
+	grid.pad_grid(0)
+
+	bead_spread = []
+	cores_ = min(os.cpu_count() - 1, args.cores)
+	chunksize, extra = divmod(coords.shape[1], cores_ * 4)
+	if extra:
+		chunksize += 1
+
+	_part_func = partial(
+		get_bead_spread,
+		coords=coords,
+		mass=mass,
+		radius=radius,
+		grid=grid,
+		voxel_size=args.voxel_size,
+		n_breaks=args.n_breaks
+	)
+
+	with Pool(args.cores) as p:
+		for spread in tqdm.tqdm(p.imap(
+			_part_func,
+			range(0, coords.shape[1] ),
+			chunksize=chunksize
+		)):
+			bead_spread.append( spread )
+
+	bead_spread = scale(bead_spread)
+	print('Bead Spread calculation done')
+	os.makedirs(output_dir, exist_ok=True)
+
+	# Save the bead_spread values.
+	if args.return_spread == 1:
+		with open(output_dir + "/bead_spreads_cl" + str(args.classes) + ".txt", "w") as fl:
+			for bs in bead_spread:
+				fl.write('{:0.3f}'.format(bs))
+				fl.write("\n")
+
+	# Obtain patches for all the beads.
+	patches = get_patches(bead_spread, args.classes, coords, radius)
+	# Annotate the patches for low-med-high precision.
+	annotated_patches = annotate_patches(patches, args.classes, ps_names, coords.shape[1])
+	high_prec, low_prec = patches[:args.classes], patches[args.classes+1:]
+
+	annot_df = pd.DataFrame(np.array(annotated_patches), columns = ['Bead', 'Bead Name', 'Type', 'Class', 'Patch'])
+	annot_df['Patch'] = pd.to_numeric(annot_df["Patch"])
+	annot_df.sort_values(['Patch'], ascending=[True])
+	annot_df.to_csv(output_dir + '/annotations_cl' + str(args.classes) + '.txt', index=None)
+
+	with open(output_dir + "/low_prec_cl" + str(args.classes) + ".txt", "w") as fl:
+		lev = 1
+		fl.write("Level" + "\t" + "Bead Indices" + "\t" + "Bead Names")
+		fl.write("\n")
+		for level in low_prec:
+			for l in level:
+				fl.write(str(lev))
+				fl.write("\t")
+				fl.write(",".join(str(item) for item in l))
+				fl.write("\t")
+				fl.write(",".join(ps_names[name] for name in l))
+				fl.write("\n")
+			lev=lev+1
+
+	with open(output_dir + "/high_prec_cl" + str(args.classes) + ".txt", "w") as fl:
+		lev = 1
+		fl.write("Level" + "\t" + "Bead Indices" + "\t" + "Bead Names")
+		fl.write("\n")
+		for level in high_prec:
+			for l in level:
+				fl.write(str(lev))
+				fl.write("\t")
+				fl.write(",".join(str(item) for item in l))
+				fl.write("\t")
+				fl.write(",".join(ps_names[name] for name in l))
+				fl.write("\n")
+			lev=lev+1
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser("PrISM")
 	parser.add_argument("--input", "-i", help="Npz file or folder containing necessary files", required=True, type=str)
-	parser.add_argument("--input_type", "-t", 
-		help="Type of input: npz/pdb/cif/rmf/dcd.", 
+	parser.add_argument("--input_type", "-t",
+		help="Type of input: npz/pdb/cif/rmf/dcd.",
 		required=True, type=str)
 	parser.add_argument("--voxel_size", "-v", help="Voxel size for density calculations", default=4, type=int)
 	parser.add_argument("--return_spread", "-rs", help="Return the spread bead_spread", action='store_true', default = True)
 	parser.add_argument("--output", "-o", help="Output directory", required = True, type=str)
-	parser.add_argument("--classes", "-cl", help="Number of classes(1,2,3)", default = 2, choices=[1,2,3], type=int) 
+	parser.add_argument("--classes", "-cl", help="Number of classes(1,2,3)", default = 2, choices=[1,2,3], type=int)
 	parser.add_argument("--cores", "-co", help="Number of cores to use", default = 16, type=int)
 	parser.add_argument("--models", "-m", help="Percentage of total models to use", default = 1, type=float)
 	parser.add_argument("--n_breaks", "-n", help="Number of breaks to use for cDist calculation", default = 50, type=int)
@@ -64,7 +160,7 @@ if __name__ == '__main__':
 		radius = arr['arr_2']
 		ps_names = arr['arr_3']
 		del arr
-		run_prism( coords, mass, radius, ps_names, args )	
+		run_prism( coords, mass, radius, ps_names, args )
 	elif args.input_type == "pdb":
 		coords, mass, radius, ps_names = parse_all_struct(args.input, _type = "pdb" )
 		run_prism( coords, mass, radius, ps_names, args )
@@ -72,6 +168,7 @@ if __name__ == '__main__':
 		coords, mass, radius, ps_names = parse_all_struct(args.input, _type = "cif" )
 		run_prism( coords, mass, radius, ps_names, args )
 	elif args.input_type == "ihm":
+		from ihm_parser import parse_ihm_models
 		parse_ihm_models( args )
 	elif args.input_type == "rmf":
 		from rmf_parser import parse_all_rmfs
@@ -81,103 +178,3 @@ if __name__ == '__main__':
 		from dcd_parser import parse_all_dcds
 		coords, mass, radius, ps_names = parse_all_dcds(args.input, args.resolution, args.subunit, args.selection)
 		run_prism( coords, mass, radius, ps_names, args )
-	
-def run_prism( coords, mass, radius, ps_names, args ):	
-	models = round(args.models*coords.shape[0])
-	if args.models != 1:
-		selected_models = np.random.choice(coords.shape[0], models, replace=False)
-		coords = coords[selected_models]
-	print("Number of Models = {}".format(coords.shape[0]))
-	print("Number of Beads = {}".format(coords.shape[1]))
-
-	# Create the grid.
-	grid = SparseGrid(voxel_size=args.voxel_size)
-	grid.create_grid(coords)
-	# Not padding the grid.
-	grid.pad_grid(0)
-	cores_ = np.min(os.cpu_count() - 1, args.cores)
-	chunksize, extra = divmod(coords.shape[1], len(cores_) * 4)
-	if extra:
-        chunksize += 1
-	with Pool(cores_) as p:
-		densities = []
-		for density in tqdm.tqdm( 
-			p.imap( 
-				partial(
-					main_density_calc,
-					coords=coords,
-					mass=mass,
-					radius=radius,
-					grid=grid,
-					voxel_size=args.voxel_size,
-					n_breaks=args.n_breaks
-				),
-				range(0, coords.shape[1]),
-				chunksize=chunksize,
-			) 
-		):
-			densities.append( density )
-	print('Density calculation done')
-
-	with Pool(cores_) as p:
-		bead_spread = []
-		for spread in tqdm.tqdm(
-			 p.imap(
-				 partial(calc_bead_spread, grid=grid),
-				 densities,
-				 chunksize=chunksize,
-			 )  
-		):
-			bead_spread.append( spread )
-	bead_spread = scale(bead_spread)
-	print('Bead Spread calculation done')
-
-	os.makedirs(args.output, exist_ok=True)
-
-	# Save the bead_spread values.
-	if args.return_spread == 1:
-		with open(args.output + "/bead_spreads_cl" + str(args.classes) + ".txt", "w") as fl:
-			for bs in bead_spread:
-				fl.write('{:0.3f}'.format(bs))
-				fl.write("\n")
-	
-	# Obtain patches for all the beads.
-	patches = get_patches(bead_spread, args.classes, coords, radius)
-	# Annotate the patches for low-med-high precision.
-	annotated_patches = annotate_patches(patches, args.classes, ps_names, coords.shape[1])
-	high_prec, low_prec = patches[:args.classes], patches[args.classes+1:]
-
-	annot_df = pd.DataFrame(np.array(annotated_patches), columns = ['Bead', 'Bead Name', 'Type', 'Class', 'Patch'])
-	annot_df['Patch'] = pd.to_numeric(annot_df["Patch"])
-	annot_df.sort_values(['Patch'], ascending=[True])
-	annot_df.to_csv(args.output + '/annotations_cl' + str(args.classes) + '.txt', index=None)
-
-	with open(args.output + "/low_prec_cl" + str(args.classes) + ".txt", "w") as fl:
-		lev = 1
-		fl.write("Level" + "\t" + "Bead Indices" + "\t" + "Bead Names")
-		fl.write("\n")
-		for level in low_prec:
-			for l in level:
-				fl.write(str(lev))
-				fl.write("\t")
-				fl.write(",".join(str(item) for item in l))
-				fl.write("\t")
-				fl.write(",".join(ps_names[name] for name in l))
-				fl.write("\n")
-			lev=lev+1
-
-	with open(args.output + "/high_prec_cl" + str(args.classes) + ".txt", "w") as fl:
-		lev = 1
-		fl.write("Level" + "\t" + "Bead Indices" + "\t" + "Bead Names")
-		fl.write("\n")
-		for level in high_prec:
-			for l in level:
-				fl.write(str(lev))
-				fl.write("\t")
-				fl.write(",".join(str(item) for item in l))
-				fl.write("\t")
-				fl.write(",".join(ps_names[name] for name in l))
-				fl.write("\n")
-			lev=lev+1
-
-